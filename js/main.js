7f3068bd43c342b5d4c84442631cd9fe5c5e20e3
ScriptKiddie404 <fernando.puentes.developer@gmail.com>
2020-11-20 10:54:29 -0700

    contact and footer features

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
 //AIzaSyDLWMPqu5pBO6WTLy1jH_gLNDi-vu2GjLM
+//AIzaSyDkO_-HqDeibG2VGEC-cerR2PCxOTTlRig
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 48.8584, lng: 2.2945 },
+    zoom: 15,
+  });
+}
